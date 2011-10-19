a6c805275ee5a89b025eddb8c2ba056f9115c260
Ella Cochran <ellacochran@rocketmail.com>
2011-10-19 20:04:41 +0200

    Only Load Libraries when used

--- modules.js
+++ modules.js
@@ -1,11 +1,22 @@
+/*
+** © 2011 by Ella Cochran <ellacochran@rocketmail.com>
+** Licensed under the MIT License
+*/
+
 (function() {
   if ('function' !== typeof window.m) {
+    var modules = {};
     var m = window.m = function(name, maker) {
       if (!name) return m.modules;
       if ('function' === typeof maker) {
+        maker.m = m;
+        modules[name] = maker;
+        return;
+      }
+      if (modules[name] && (modules[name].m == m)) {
         m.exports = {};
-        maker(m.exports, m);
-        m.modules[name] = m.exports;
+        modules[name](m.exports, m);
+        modules[name] = m.exports;
         m.exports = undefined;
       }
       return m.modules[name];
